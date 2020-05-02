eb1f4b62fbb2729a7f84fa0e189f868516c5cb85
msyphus <mark.syphus@gmail.com>
2020-05-01 21:16:38 -0600

    stylized menu

--- assets/javascript/run.js
+++ assets/javascript/run.js
@@ -3,15 +3,15 @@ var setSiteMode = "light";
 
 $("#menuIcon").on("click", function() {
     if(menuMinimized === true) {
-        $("#menuIcon").css("margin-left", "210px");
-        $("#sideNav").css("width", "200px");
+        $("#menuIcon").css("margin-left", "12%");
+        $("#sideNav").css("width", "11%");
         $(".barMiddle").hide();
         $(".barTop").css("transform", "rotate(45deg) translate(-1px, 10px)");
         $(".barBottom").css("transform", "rotate(-45deg) translate(-6px, -6px)");
         menuMinimized = false;
     } else {
         $("#sideNav").css("width", "0");
-        $("#menuIcon").css("margin-left", "0");
+        $("#menuIcon").css("margin-left", "1%");
         $(".barMiddle").show();
         $(".barTop").css("transform", "rotate(0deg)");
         $(".barBottom").css("transform", "rotate(0deg)");
