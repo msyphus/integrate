4367f1df01e1f0275eca066f5de219eb14f4493f
msyphus <mark.syphus@gmail.com>
2020-05-01 19:14:11 -0600

    animated menu icon to close button and vice verse

--- assets/javascript/run.js
+++ assets/javascript/run.js
@@ -1,13 +1,19 @@
-// $(document).on("ready", function() {
-//     $("#sideNav").hide();
-// })
+var menuMinimized = true;
 
 $("#menuIcon").on("click", function() {
-    // $("#sideNav").show();
-    $("#menuIcon").css("margin-left", "210px")
-    $("#sideNav").css("width", "200px")
-})
-
-$(".closeButton").on("click", function() {
-    $("#sideNav").css("width", "0");
-})
+    if(menuMinimized === true) {
+        $("#menuIcon").css("margin-left", "210px");
+        $("#sideNav").css("width", "200px");
+        $(".barMiddle").hide();
+        $(".barTop").css("transform", "rotate(45deg) translate(-1px, 10px)");
+        $(".barBottom").css("transform", "rotate(-45deg) translate(-6px, -6px)");
+        menuMinimized = false;
+    } else {
+        $("#sideNav").css("width", "0");
+        $("#menuIcon").css("margin-left", "0");
+        $(".barMiddle").show();
+        $(".barTop").css("transform", "rotate(0deg)");
+        $(".barBottom").css("transform", "rotate(0deg)");
+        menuMinimized = true;
+    }
+});
