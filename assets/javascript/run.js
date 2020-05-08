6c0db5e0c26e01398df88a704c8ee8010d65b7f2
msyphus <mark.syphus@gmail.com>
2020-05-07 23:49:24 -0600

    fixed menu bar pixel transformation issue and footer responsiveness

--- assets/javascript/run.js
+++ assets/javascript/run.js
@@ -15,15 +15,15 @@ $("#menuIcon").on("click", function() {
         $("#menuIcon").css("margin-left", "110px");
         $("#sideNav").css("width", "100px");
         $(".barMiddle").hide();
-        $(".barTop").css("transform", "rotate(45deg) translate(-1px, 10px)");
-        $(".barBottom").css("transform", "rotate(-45deg) translate(-6px, -6px)");
+        $(".barTop").css({"transform": "rotate(45deg)", "top": "10px"});
+        $(".barBottom").css({"transform": "rotate(-45deg)", "top": "10px"});
         menuMinimized = false;
     } else {
         $("#sideNav").css("width", "0");
         $("#menuIcon").css("margin-left", "1%");
         $(".barMiddle").show();
-        $(".barTop").css("transform", "rotate(0deg)");
-        $(".barBottom").css("transform", "rotate(0deg)");
+        $(".barTop").css({"transform": "rotate(0deg)", "top": "0"});
+        $(".barBottom").css({"transform": "rotate(0deg)", "top": "14px"});
         menuMinimized = true;
     }
 });
